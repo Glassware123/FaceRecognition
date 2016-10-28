7d23af87a11ef2e3e2d8c576b62557177b4e5993
Glassware123 <anilbas@gmail.com>
2016-10-28 18:36:21 +0300

    face recognation script with python and opencv

---
+++ base.py
@@ -0,0 +1,66 @@
+import cv2, os
+import numpy as np
+from PIL import Image
+import sys
+
+
+faceCascade = cv2.CascadeClassifier("haarcascade_frontalface_default.xml")
+eyeCascade = cv2.CascadeClassifier("haarcascade_eye.xml")
+video_capture = cv2.VideoCapture(0)
+recognizer = cv2.createLBPHFaceRecognizer()
+
+
+def get_images_and_labels(path):
+     image_paths = [os.path.join(path, f) for f in os.listdir(path)]
+     images = []
+     labels = []
+     for image_path in image_paths:
+         image_pil = Image.open(image_path).convert('L')
+         image = np.array(image_pil, 'uint8')
+         name = int(os.path.split(image_path)[1].split(".")[0].replace("subject", ""))
+         faces = faceCascade.detectMultiScale(image)
+         for (x, y, w, h) in faces:
+             images.append(image[y: y + h, x: x + w])
+             labels.append(name)
+             cv2.imshow("Adding faces to traning set...", image[y: y + h, x: x + w])
+             cv2.waitKey(50)
+     return images, labels
+
+images, labels = get_images_and_labels("faceDatabase")
+recognizer.train(images, np.array(labels))
+
+while True:
+    ret, frame = video_capture.read()
+
+    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+
+    faces = faceCascade.detectMultiScale(
+        gray,
+        scaleFactor=1.1,
+        minNeighbors=5,
+        minSize=(30, 30),
+        flags=cv2.cv.CV_HAAR_SCALE_IMAGE
+    )
+
+    # Draw a rectangle around the faces
+    for (x, y, w, h) in faces:
+        nbr_predicted, conf = recognizer.predict(gray[y:y+h, x:x+w])
+        cv2.rectangle(frame, (x, y), (x+w, y+h), (0, 255, 255), 2)
+        print nbr_predicted, conf
+        roi_gray = gray[y:y+h, x:x+w]
+        roi_color = frame[y:y + h, x:x + w]
+        eyes = eyeCascade.detectMultiScale(roi_gray)
+        for (ex, ey, ew, eh) in eyes:
+            cv2.rectangle(roi_color, (ex, ey), (ex + ew, ey + eh), (0, 255, 0), 2)
+
+
+    # Display the resulting frame
+    cv2.imshow('Video', frame)
+
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
+
+
+# When everything is done, release the capture
+video_capture.release()
+cv2.destroyAllWindows()
